0974213cf5cc998502825fefc9df5d61640e8f71
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-11-20 23:54:43 -0500

    Roll back to Next 9.0.6

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,11 +1,17 @@
-const functions = require('firebase-functions');
-
 'use strict';
 
+const functions = require('firebase-functions');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
+
 const app = next({dev, conf: {distDir: 'next'}});
+
+// var app = next({
+//     dev: false,
+//     conf: { distDir: `${path.relative(process.cwd(), __dirname)}/next` }
+//   })
+
 const handle = app.getRequestHandler();
 
 exports.next = functions.https.onRequest(async (req, res) => {
