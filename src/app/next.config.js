c0735e61ca35ea12034769e56fe264e6ef498394
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-11-20 22:40:07 -0500

    Next Config

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,13 +1,22 @@
-const withSass = require("@zeit/next-sass");
-module.exports.withSass = withSass();
 
-module.exports = withSass({
-	module: {
-		rules: [
-			{
-				test: /\.md$/i,
-				use: "raw-loader"
-			}
-		]
-	}
-});
+'use strict';
+
+module.exports = {
+  distDir: '../functions/next'
+};
+
+
+
+// const withSass = require("@zeit/next-sass");
+// module.exports.withSass = withSass();
+
+// module.exports = withSass({
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.md$/i,
+// 				use: "raw-loader"
+// 			}
+// 		]
+// 	}
+// });
