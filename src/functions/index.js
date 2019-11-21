6028eacf575980e34d98518bd4d4f4c38c2fb963
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-11-21 01:35:27 -0500

    Minor

--- src/functions/index.js
+++ src/functions/index.js
@@ -7,7 +7,7 @@ const dev = process.env.NODE_ENV !== 'production';
 
 // const app = next({dev, conf: {distDir: 'next'}});
 
-const app = next({dir:'./src/app', dev});
+const app = next({dir:'./src', dev});
 
 // var app = next({
 //     dev: false,
