528965588109047c6c64db5ed4fac1f0eaf182dd
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-11-24 00:11:21 -0500

    Major Restructuring and Cleanup

--- next.config.js
+++ next.config.js
@@ -22,17 +22,3 @@ module.exports = withCSS(withSass({
        return config;
    }
 }));
-
-// const withSass = require("@zeit/next-sass");
-// module.exports.withSass = withSass();
-
-// module.exports = withSass({
-// 	module: {
-// 		rules: [
-// 			{
-// 				test: /\.md$/i,
-// 				use: "raw-loader"
-// 			}
-// 		]
-// 	}
-// });
