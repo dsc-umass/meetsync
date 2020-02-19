e3df637e10dcc4eb6964e13e97dae5fa67927a6b
Timothy Shee <timothy_shee@hotmail.com>
2020-02-19 11:42:34 -0500

    Dashboard - Event List Functionality
    Requires styling update

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,22 +1,143 @@
 // pages/dashboard.js
 import React from 'react';
 
+import { auth, firebase } from '../components/Firebase/firebase'
 // Add this at the top of the page
 import withAuth from '../functions/withAuth';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
+import router from 'next/router';
+import axios from 'axios';
+import ReactLoading from 'react-loading';
+import Link from 'next/link';
+import { Button } from 'reactstrap';
 
-class Dashboard extends React.Component {
- render() {
-   return (
+import { MDBIcon } from 'mdbreact';
+//<i class="fas fa-trash-alt"></i>
+//<MDBIcon icon="trash-alt" />
+
+const Example = ({ type, color }) => (
+  <ReactLoading type={type} color={color} height={667} width={375} />
+);
+
+//import EventForm from '../components/EventForm'
+//import EventList from '../components/EventList'
+
+const Todo = ({todo, remove}) => {
+  // Each Todo
+  return (<li>{todo.text}</li>);
+}
+
+const TodoList = ({todos, remove}) => {
+  // Map through the todos
+  const todoNode = todos.map((todo) => {
+    return (<Todo todo={todo} key={todo.id} remove={remove}/>)
+  });
+  return (<ul>{todoNode}</ul>);
+}
+
+const Event = ({event, remove}) => {
+  console.log(event)
+  console.log(event.id)
+  return (
+    <li>
+      <div className="row">
+        <div className="col">
+          <Link href="/p/[id]" as={`/p/${event.id}`}>
+            <a style={{fontSize: 20}}>{event.title}</a>
+          </Link>
+        </div>
+        <div>
+          <Button onClick={remove}>
+            <MDBIcon icon="trash-alt" />
+          </Button>
+        </div>
+      </div>
+    </li>);
+}
+
+const EventsList = ({eventsObj, remove}) => {
+  // Map through the todos
+  const todoNode = eventsObj.map((event, remove) => {
+    return (<Event event={event} remove={remove}/>)
+  });
+  return (<ul>{todoNode}</ul>);
+}
+
+class Dashboard extends React.Component{
+  constructor(props){
+    // Pass props to parent class
+    super(props);
+    // Set initial state
+    this.state = {
+      data: [],
+      shows: [],
+      promiseIsResolved: false
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api/getAllEvents')
+      .then((res) => {
+        console.log(res.data)
+        this.setState({
+          promiseIsResolved: true,
+          shows: res.data
+        })
+      });
+  }
+
+  // Handle remove
+  handleRemove(id){
+    // Filter all todos except the one to be removed
+    const remainder = this.state.data.filter((todo) => {
+      if(todo.id !== id) return todo;
+    });
+    // Update state with filter
+    axios.delete('/api/deleteEvent?id='+id)
+      .then((res) => {
+        this.setState({data: remainder});      
+      })
+  }
+  
+
+  handleLogout = () => {
+    auth.signOut().then(function () {
+        alert('Logout successful');
+    }).catch(function (error) {
+        alert('OOps something went wrong check your console');
+        console.log(err);
+    });
+    router.push('/')
+  }
+
+  render(){
+    console.log(this.state.shows)
+    if(!this.state.promiseIsResolved){return (
       <Layout>
-        <section className="text-center">
-          <div className="container">
-            <h1>Dashboard  Page</h1>
-            <p>You can't go into this page if you are not authenticated.</p>
+        <section>
+          <div style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}>
+              <Example type={"balls"} color={"#ffffff"} />
           </div>
         </section>
       </Layout>
-     )
-   }
+    )}
+    return (
+      <Layout>
+        <section>
+          <div className="container">
+            <h1 className="text-center">Dashboard</h1>
+            <EventsList eventsObj={this.state.shows} remove={this.handleRemove.bind(this)}/>
+            <button onClick={this.handleLogout}>Logout</button>
+          </div>
+          <MDBIcon icon="trash-alt" />
+        </section>
+        
+      </Layout>
+    );
+  }
 }
 export default withAuth(Dashboard);
