0a5ca1a060e65d16d8e9512ed534e0f6a21558ca
Timothy Shee <timothy_shee@hotmail.com>
2020-05-15 09:07:56 -0400

    Added calendar (after quite a few edits)
    https://blog.flowandform.agency/create-a-custom-calendar-in-react-3df1bfd0b728

---
+++ components/Calendar.js
@@ -0,0 +1,136 @@
+import React from "react";
+import {
+    format,
+    startOfWeek,
+    endOfWeek,
+    startOfMonth,
+    endOfMonth,
+    startOfYear,
+    endOfYear,
+    addDays,
+    addMonths,
+    isSameDay,
+    isSameMonth,
+    subMonths,
+    parse
+} from "date-fns";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faChevronLeft,
+    faChevronRight
+  } from "@fortawesome/free-solid-svg-icons";
+
+import './Calendar.css'
+class Calendar extends React.Component {
+  state = {
+    currentMonth: new Date(),
+    selectedDate: new Date()
+  };
+
+  renderHeader() {
+    const dateFormat = "MMMM yyyy";
+
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+            <FontAwesomeIcon icon={faChevronLeft} className="mr-2" onClick={this.prevMonth}/>
+        </div>
+        <div className="col col-center">
+          <span>{format(this.state.currentMonth, dateFormat)}</span>
+        </div>
+            <FontAwesomeIcon icon={faChevronRight} className="mr-2" onClick={this.nextMonth}/>
+      </div>
+    );
+  }
+
+  renderDays() {
+    const dateFormat = "dddd";
+    const days = [];
+
+    let startDate = startOfWeek(this.state.currentMonth);
+
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {format(addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    }
+
+    return <div className="days row">{days}</div>;
+  }
+
+  renderCells() {
+    const { currentMonth, selectedDate } = this.state;
+    const monthStart = startOfMonth(currentMonth);
+    const monthEnd = endOfMonth(monthStart);
+    const startDate = startOfWeek(monthStart);
+    const endDate = endOfWeek(monthEnd);
+
+    const dateFormat = "d";
+    const rows = [];
+
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = format(day, dateFormat);
+        const cloneDay = day;
+        days.push(
+          <div
+            className={`col cell ${
+              !isSameMonth(day, monthStart)
+                ? "disabled"
+                : isSameDay(day, selectedDate) ? "selected" : ""
+            }`}
+            key={day}
+            onClick={() => this.onDateClick(parse(cloneDay))}
+          >
+            <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+          </div>
+        );
+        day = addDays(day, 1);
+      }
+      rows.push(
+        <div className="row" key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className="body">{rows}</div>;
+  }
+
+  onDateClick = day => {
+    this.setState({
+      selectedDate: day
+    });
+  };
+
+  nextMonth = () => {
+    this.setState({
+      currentMonth: addMonths(this.state.currentMonth, 1)
+    });
+  };
+
+  prevMonth = () => {
+    this.setState({
+      currentMonth: subMonths(this.state.currentMonth, 1)
+    });
+  };
+
+  render() {
+    return (
+      <div className="calendar">
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
+      </div>
+    );
+  }
+}
+
+export default Calendar;
--- pages/dashboard/events.js
+++ pages/dashboard/events.js
@@ -1,6 +1,48 @@
+// pages/dashboard.js
 import React from 'react';
+
+// Add this at the top of the page
 import withAuth from '../../functions/withAuth';
+import Layout from '../../components/MainLayout'
 import DashboardLayout from '../../components/DashboardLayout';
+import router from 'next/router';
+import axios from 'axios';
+import ReactLoading from 'react-loading';
+import Link from 'next/link';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
+import {
+  Button, Card, CardHeader, CardBody,
+  Container, Row, Col
+} from 'reactstrap'
+
+import Calendar from '../../components/Calendar'
+
+const LoadingComponent = ({ type, color }) => (
+  <ReactLoading type={type} color={color} height={667} width={375} />
+);
+
+const Event = ({event, remove}) => {
+  return (
+    <li>
+      <div className="form-inline">
+        <Link href="/p/[id]" as={`/p/${event.id}`}>
+          <a style={{fontSize: 20}}>{event.title}</a>
+        </Link>
+        <Button onClick={remove}>
+          <FontAwesomeIcon icon={faTrash} className="mr-2" />
+        </Button>
+      </div>
+    </li>);
+}
+
+const EventsList = ({eventsObj, remove}) => {
+  // Map through the todos
+  const todoNode = eventsObj.map((event, remove) => {
+    return (<Event event={event} remove={remove}/>)
+  });
+  return (<ul>{todoNode}</ul>);
+}
 
 class Events extends React.Component{
   constructor(props){
@@ -8,13 +50,72 @@ class Events extends React.Component{
     super(props);
     // Set initial state
     this.state = {
+      data: [],
+      shows: [],
+      promiseIsResolved: false
     }
   }
 
-  render() {
+  componentDidMount() {
+    axios.get('/api/getAllEvents')
+      .then((res) => {
+        console.log(res.data)
+        this.setState({
+          promiseIsResolved: true,
+          shows: res.data
+        })
+      });
+  }
+
+  // Handle remove
+  handleRemove(id){
+    // Filter all todos except the one to be removed
+    const remainder = this.state.data.filter((todo) => {
+      if(todo.id !== id) return todo;
+    });
+    // Update state with filter
+    axios.delete('/api/deleteEvent?id='+id)
+      .then((res) => {
+        this.setState({data: remainder});      
+      })
+  }
+
+  createNewEvent = () => {
+    router.push('/create_event');
+  }
+
+  render(){
+    console.log(this.state.shows)
+    if(!this.state.promiseIsResolved){return (
+      <Layout>
+        <section>
+          <div style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}>
+              <LoadingComponent type={"balls"} color={"#ffffff"}/>
+          </div>
+        </section>
+      </Layout>
+    )}
     return (
       <DashboardLayout>
         <h1>Events</h1>
+        <Container>
+          <Row>
+            <Col xs="6">
+              <Card>
+                <CardHeader><h3>Event Listing</h3></CardHeader>
+                <CardBody><EventsList eventsObj={this.state.shows} remove={this.handleRemove.bind(this)}/></CardBody>
+              </Card>
+            </Col>
+            <Col xs="6">
+              <Calendar />
+            </Col>
+          </Row>
+        </Container>
+        
       </DashboardLayout>
     );
   }
