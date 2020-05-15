117984f48c11644ac290dfdf80e617f3fc47be3e
Timothy Shee <timothy_shee@hotmail.com>
2020-05-15 07:44:26 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/MainLayout";
-import { Container, Row, Col } from 'reactstrap';
-import Link from "next/link";
-import { auth, firebase } from '../components/Firebase/firebase'
+import { Container, Row, Col, Button } from 'reactstrap';
 import router from "next/router";
+import Link from "next/link"
 import Landing from '../components/Landing'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faDonate } from "@fortawesome/free-solid-svg-icons";
 
 export default class Home extends React.Component {
 	constructor(props) {
@@ -40,7 +42,31 @@ export default class Home extends React.Component {
             <p>Project created for school clubs to set up regular meetings for the coming semester</p>
             <p>enables you to effortlessly schedule club meetings</p>
             <p>Brought to you by the UMass DSC Developers</p>
-            <img src="/assets/acm_umass.jpg" alt="UMass Image" />
+            <img src="/assets/acm_umass.jpg" alt="UMass Image" style={{maxWidth:"880px"}}/>
+            <div style={{alignItems: "center", display: "flex",}}>
+              <Row>
+                <>
+                  <Link href="/signup">
+                    <Button color="primary">Try It</Button>
+                  </Link>
+                </>
+                <>
+                  <a href="https://www.patreon.com/" target="_blank">
+                    <Button color="success">
+                      <FontAwesomeIcon icon={faDonate} className="mr-2" /> Donate
+                    </Button>
+                  </a>
+                </>
+                <>
+                  <a href="https://github.com/dsc-umass/meetsync" target="_blank">
+                    <Button color="secondary">
+                      <FontAwesomeIcon icon={faGithub} className="mr-2" /> GitHub
+                    </Button>
+                  </a>
+                </>
+              </Row>
+            </div>
+            
           </Container>
 				</section>
 			</Layout>
