b9a51122995d5e6d85b5f4a8d8941ed13e417e62
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-05-17 22:45:18 -0400

    cleaning up for new rails setup

--- pages/api/deleteEvent.js
+++
@@ -1,5 +0,0 @@
-import events from './events.json';
-export default (req, res) => {
-    //need to send delete request to database
-    res.status(200).json(events[req.query.id]);
-  };
--- pages/api/getAllEvents.js
+++
@@ -1,5 +0,0 @@
-import events from './events.json';
-export default (req, res) => {
-    //const event = events[Math.floor(Math.random() * events.length)];
-    res.status(200).json(events);
-  };
